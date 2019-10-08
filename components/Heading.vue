e57adda6bd7aac970ec1e04f57579ccb7c530ce1
harutete <ai.takeuchi.10@gmail.com>
2019-10-08 22:02:50 +0900

    環境構築

---
+++ components/Heading.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>hoge!</h1>
+</template>
+
+<script lang="ts">
+export default ({
+    data () {
+        return {
+            title: 'hoge!'
+        }
+    }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello Vue</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import axios from 'axios'
+    import HeadingHoge from '~/components/Heading.vue'
+
+    export default ({
+        // async created () {
+        //     const data = await axios.get('')
+
+        //     console.log(data)
+        // }
+    })
+</script>
