581a40c7019d803be135c846a0c009de2d0b53c6
harutete <ai.takeuchi.10@gmail.com>
2019-10-28 08:08:00 +0900

    　cookie調整

--- plugins/cookieStorage.ts
+++ plugins/cookieStorage.ts
@@ -4,14 +4,14 @@ import cookie from 'cookie'
 
 export default ({ store, req, isDev }) => {
   createPersistedState({
-    key: 'words-key',
+    key: 'my-key',
     paths: [
-      'count'
+        "count"
     ],
     storage: {
-      getItem: (key) => process.client ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie || '')[key],
-      setItem: (key, value) => Cookies.set(key, value, { expires: 365, secure: !isDev }),
-      removeItem: key => Cookies.remove(key)
+        getItem: (key) => process.client ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie || '')[key],
+        setItem: (key, value) => Cookies.set(key, value, { expires: 365, secure: !isDev }),
+        removeItem: (key) => Cookies.remove(key)
     }
-  })
+  })(store)
 }
