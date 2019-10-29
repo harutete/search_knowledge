f01cc9ea9f5d1d32620b7eecab43eac2d17dbb86
harutete <ai.takeuchi.10@gmail.com>
2019-10-29 19:18:27 +0900

    cookie取得方法変更

--- plugins/cookieStorage.ts
+++ plugins/cookieStorage.ts
@@ -1,16 +1,23 @@
 import createPersistedState from 'vuex-persistedstate'
 import * as Cookies from 'js-cookie'
+import cookie from 'cookie'
 
-const day = 7
-export default ({ store }) => {
-  modules: {
-    wordHistory
-  },
-  strict: true,
-  plugins: [createPersistedState({
-    key: 'wordList',
-    paths: [
-      'wordHistory'
-    ],
-  })]
+export default ({ store, req, isDev }) => {
+  const day = 7
+
+  createPersistedState({
+    key: 'words',
+    storage: {
+      getItem: (key: string) => {
+        if (process.client) {
+          Cookies.getJSON(key)
+        } else {
+          cookie.parse(req.headers.cookie || '')[key]
+        }
+      },
+      setItem: (key: string, value: any) =>
+        Cookies.set(key, value, { expires: day, secure: !isDev }),
+      removeItem: (key) => Cookies.remove(key)
+    }
+  })(store)
 }
